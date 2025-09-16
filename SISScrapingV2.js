1700796779796f13bbb4a94b27d81cc8bcfd40a9
Huy Nguyen <91553769+nhathuy07@users.noreply.github.com>
2025-09-17 00:22:05 +0700

    Add files via upload

--- SISScrapingV2.js
+++ SISScrapingV2.js
@@ -25,17 +25,17 @@ while (new Date(document.querySelectorAll("h3")[1].innerText.replace(/,/g, "")).
                 let end_m = parseInt(end_hhmm_string[1])
 
                 let start_js_date = new Date( new Date(current_date).setHours(start_h, start_m) )
-                let end_js_date = new Date( new Date(current_date).setHours(start_h, start_m) )
+                let end_js_date = new Date( new Date(current_date).setHours(end_h, end_m) )
 
-                let ics_string_start = `DTSTART;TZID=Asia/Ho_Chi_Minh:${start_js_date.getFullYear()}${ String( start_js_date.getMonth()+1).padStart(2, '0') }${ String( start_js_date.getDate()).padStart(2, '0')}T${start_js_date.getHours()}:${start_js_date.getMinutes()}00`
+                let ics_string_start = `DTSTART;TZID=Asia/Ho_Chi_Minh:${start_js_date.getFullYear()}${ String( start_js_date.getMonth()+1).padStart(2, '0') }${ String( start_js_date.getDate()).padStart(2, '0')}T${  String( start_js_date.getHours()).padStart(2, '0')  }${ String( start_js_date.getMinutes()).padStart(2, '0')}00`
 
-                let ics_string_end = `DTEND;TZID=Asia/Ho_Chi_Minh:${end_js_date.getFullYear()}${ String( end_js_date.getMonth()+1 ).padStart(2, '0')}${ String( end_js_date.getDate() ).padStart(2, '0') }T${end_js_date.getHours()}:${end_js_date.getMinutes()}00`
+                let ics_string_end = `DTEND;TZID=Asia/Ho_Chi_Minh:${end_js_date.getFullYear()}${ String( end_js_date.getMonth()+1 ).padStart(2, '0')}${ String( end_js_date.getDate() ).padStart(2, '0') }T${ String( end_js_date.getHours()).padStart(2, '0')}${ String(end_js_date.getMinutes()).padStart(2, '0')}00`
 
                 //     TODO: Get event name
                 cal.push({start: ics_string_start, end: ics_string_end, eventName: ev_name})
             } else {
-                let ics_string_start = `DTSTART;VALUE=DATE:${current_date.getFullYear()}${current_date.getMonth()+1}${current_date.getDate()}`
-                let ics_string_end = `DTEND;VALUE=DATE:${current_date.getFullYear()}${current_date.getMonth()+1}${current_date.getDate()}`
+                let ics_string_start = `DTSTART;VALUE=DATE:${current_date.getFullYear()}${ String( current_date.getMonth()+1).padStart(2, '0')}${ String( current_date.getDate()).padStart(2, '0')}`
+                let ics_string_end = `DTEND;VALUE=DATE:${current_date.getFullYear()}${ String( current_date.getMonth()+1).padStart(2, '0')}${ String( current_date.getDate()).padStart(2, '0')}`
 
 //                 All day events
                 cal.push({start: ics_string_start, end: ics_string_end, eventName: ev_name})
@@ -58,7 +58,7 @@ let ics_file_end = "\n\nEND:VCALENDAR"
 let ics_stream = ics_file_start
 
 let cdate = new Date()
-let dtstamp = `DTSTAMP:${cdate.getFullYear()}${cdate.getMonth()}${cdate.getDate()}T${cdate.getHours()}${cdate.getMinutes()}${cdate.getSeconds()}`
+let dtstamp = `DTSTAMP;TZID=Asia/Ho_Chi_Minh:${cdate.getFullYear()}${ String( cdate.getMonth()).padStart(2, '0') }${ String( cdate.getDate()).padStart(2, '0')}T000000`
 
 for (let calevent of cal) {
     const id = crypto.randomUUID()
